97a30c63fd3cc6a93ab39c7ffb564fa38ff9c9a9
bzy <bangziyue@huodongxing.com>
2022-02-14 18:50:48 +0800

    ConfigApplication7070

--- bzy-cloud-consumer80/src/main/java/com/consumer/config/ApplicationContextBean.java
+++ bzy-cloud-consumer80/src/main/java/com/consumer/config/ApplicationContextBean.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class CommonConfig {
+public class ApplicationContextBean  {
 
     /**
      * 初始化 RestTemplate
